b4186a1f6606de01d5e3773c5475a79a94e33ece
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-10-19 17:52:44 -0300

    session beans

---
+++ despacho.backend/ejbModule/services/AdministradorArticulosBean.java
@@ -0,0 +1,13 @@
+package services;
+
+public class AdministradorArticulosBean {
+	
+	public void guardarArticuloNuevo(Object articulo) {
+		// Se debe registrar dicho artículo junto con la información del Módulo Depósito.
+	}
+	
+	public void guardarArticuloADespachar() {
+		// Registra la Recepción de Stock y se debe procesar las Ordenes de Despacho Pendientes de Entrega. 
+		// Se deberá procesar primero las Órdenes de Despacho más antiguas.
+	}
+}
---
+++ despacho.backend/ejbModule/services/AdministradorOrdenesDespachoBean.java
@@ -0,0 +1,17 @@
+package services;
+
+public class AdministradorOrdenesDespachoBean {
+	
+	public void guardarOrden(Object ordenDespacho) {
+		// - Las órdenes de Despacho deben ser registradas en el sistema como pendientes de Entrega.
+		// - Por cada artículo, se debe obtener el Deposito que lo administra y solicitarlo asincrónicamente
+		// - Se debe registrar la solicitud de articulo por Deposito
+	}
+	
+	public void cambiarEstado() {
+		// Si el nuevo estado de la orden de despacho es "lista para entregar", se deberá:
+		//	- Informar en comunicación sincrónica a los Portales.
+		//  - Informar en comunicación sincrónica utilizando tecnología REST y JSON al módulo Logística
+		// El sistema debe registrar y cambiar de estado a la Orden de Despacho y marcarla como entregada
+	}
+}
