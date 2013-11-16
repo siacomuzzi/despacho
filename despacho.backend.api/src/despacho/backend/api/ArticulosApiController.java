e267ed116e334f38fe5f3fb71f15a4dc7721da6f
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-11-16 09:19:12 -0300

    BusinessDelegate

--- despacho.backend.api/src/despacho/backend/api/ArticulosApiController.java
+++ despacho.backend.api/src/despacho/backend/api/ArticulosApiController.java
@@ -1,6 +1,5 @@
 package despacho.backend.api;
 
-import javax.ejb.EJB;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -8,22 +7,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import despacho.backend.servicios.*;
-
 @Path("/articulos")
 public class ArticulosApiController {
 	
-	@EJB
-	private ServicioArticulos servicioArticulos;
-	
 	@POST
-	@Path("/paraDespachar") 
+	@Path("/paraDespachar")
 	@Consumes(MediaType.APPLICATION_JSON)
 	// DCH03.Recepción y Procesamiento de Artículos a Despachar
 	public Response recepcionArticulosParaDespachar() throws NamingException {
 		
 		// TODO: definir que parametros recibe
-		servicioArticulos.recepcionArticulosParaDespachar();
+		BusinessDelegate.getInstance().recepcionArticulosParaDespachar();
 		
 		return Response.status(200).entity("Recepcion procesada exitosamente!").build();
 	}
---
+++ despacho.backend.api/src/despacho/backend/api/BusinessDelegate.java
@@ -0,0 +1,69 @@
+package despacho.backend.api;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ar.edu.uade.integracion.VO.ArticuloVO;
+import despacho.backend.servicios.ServicioArticulos;
+
+public class BusinessDelegate implements ServicioArticulos {
+
+	static BusinessDelegate instancia = null;
+	ServicioArticulos servicioArticulos;
+	
+	public static BusinessDelegate getInstance() throws NamingException {
+		if (instancia == null) {
+			instancia= new BusinessDelegate();
+		}
+		
+		return instancia;
+	}
+	
+	private BusinessDelegate() throws NamingException {
+
+		try {
+			final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
+			jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+			jndiProperties.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", "false");
+			jndiProperties.put("remote.connections", "default");
+			jndiProperties.put("remote.connection.default.host", "localhost");
+			jndiProperties.put("remote.connection.default.port", "4447");
+			jndiProperties.put("remote.connection.default.connect.options.org.xnio.Options.SASL_POLICY_NOANONYMOUS", "false");
+
+			final Context context = new InitialContext(jndiProperties);
+
+			final String earAppName = "despacho.backendEAR";
+			final String ejbModuleName = "despacho.backend";
+			final String distinctName = "";
+			final String beanName = "ServicioArticulosBean";
+			final String viewClassName = ServicioArticulos.class.getName();
+			String url = 
+					"ejb:" + earAppName + "/" + ejbModuleName + "/" + distinctName + "/" + beanName + "!" + viewClassName;
+
+			System.out.println("Looking EJB via JNDI ");
+			System.out.println(url);
+
+			servicioArticulos = (ServicioArticulos) context.lookup(url);
+
+		} catch (Exception e) {
+			e.getCause();
+			System.out.println("Entro en exception");
+		}
+	}
+	
+	@Override
+	public void ingresarArticulo(ArticuloVO articulo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recepcionArticulosParaDespachar() {
+		// TODO Auto-generated method stub
+		servicioArticulos.recepcionArticulosParaDespachar();
+	}
+
+}
